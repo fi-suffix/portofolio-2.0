549d3025304ec2540eb69a66008a310d3998a931
fi-suffix <fadildafiamsyah@gmail.com>
2026-05-09 14:30:00 +0700

    update

--- app.js
+++ app.js
@@ -20,3 +20,14 @@ function togglesubMenu(button) {
     toogleButton.classList.toggle("rotate");
   }
 }
+
+// scroll alus
+document.querySelectorAll('nav a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+});
